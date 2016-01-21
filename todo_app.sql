90b603838be5a63cd4af448dc4ba493d90f17123
Micah Koki <micah.koki@gmail.com>
2016-01-20 16:24:34 -1000

    Step 3 and 4 done

--- todo_app.sql
+++ todo_app.sql
@@ -5,3 +5,9 @@ DROP USER IF EXISTS michael;
 -- 2 create a user named 'michael' with encrypted password 'stonebreaker'
 CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 
+-- 3 drop a database named 'todoapp' if it exists
+DROP DATABASE IF EXISTS todoapp;
+
+-- 4 create a database named todoapp
+CREATE DATABASE todoapp;
+
